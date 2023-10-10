1c5b8b33978d9dc0fc06a805e8d1fecfb968abdd
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-10-10 12:50:01 +0100

    fix: remove spaces from validation data

--- models/userModel.go
+++ models/userModel.go
@@ -8,13 +8,13 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `bson:"_id"`
-	FirstName    *string            `json:"first_name" validate:"required, min=2, max=50"`
-	LastName     *string            `json:"last_name" validate:"required, min=2, max=50"`
-	Email        *string            `json:"email" validate:"email, required"`
+	FirstName    *string            `json:"first_name" validate:"required,min=2,max=50"`
+	LastName     *string            `json:"last_name" validate:"required,min=2,max=50"`
+	Email        *string            `json:"email" validate:"email,required"`
 	Password     *string            `json:"password" validate:"required"`
 	Phone        *string            `json:"phone" validate:"required"`
 	Token        *string            `json:"token"`
-	UserType     *string            `json:"user_type" validate:"required, eq=USER|eq=ADMIN"`
+	UserType     *string            `json:"user_type" validate:"required,eq=USER|eq=ADMIN"`
 	RefreshToken *string            `json:"refresh_token"`
 	CreatedAt    time.Time          `json:"created_at"`
 	UpdatedAt    time.Time          `json:"updated_at"`
